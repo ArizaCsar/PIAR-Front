3298c104b0627e0004a8ac93fb357898e65aba6f
Yeison <yeison.restrepo.r@gmail.com>
2020-10-21 18:53:25 -0500

    Inclusión de relaciones en modelos y nuevas columnas en crud

--- src/app/models/ciudad.ts
+++ src/app/models/ciudad.ts
@@ -1,5 +1,8 @@
+import { Departamento } from './departamento.model';
+
 export interface Ciudad {
     codigoCiudad: string;
     descripcionCiudad: string;
     codigoDepartamento:string;
+    departamento: Departamento | any;
   }
